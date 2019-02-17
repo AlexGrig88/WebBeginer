3ca64198dd5900c6db9c072177258294509bf10d
Alexandr Grig <sacharost88@mail.ru>
2019-02-17 20:32:04 +0300

    my first commit

---
+++ script.js
@@ -0,0 +1,24 @@
+'use strict';
+
+//console.log(second);
+var leftBorderWidth = 1;
+
+let second = 2;
+
+const pi = 3.14;
+console.log(leftBorderWidth);
+console.log(second);
+
+let persone = {
+    name : "Ivan",
+    age : 33,
+    isMarried : false
+};
+console.log(persone.name);
+
+// let arr = ['home.png', 'js.bmp', 'swt.gif'];
+// console.log(arr[0]);
+// let answer = confirm("Are you here?");
+// console.log(answer);
+//let name2 = prompt("Как зовут вашего попугая?", "Кеша");
+//console.log(typeof(name2));
